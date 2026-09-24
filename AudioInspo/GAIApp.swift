529df9683f1eb179b67fbad1fa8b2e552c4f127c
Gale W <mail@galewilliams.com>
2026-09-24 11:42:41 -0400

    misc: Skeleton: Getting there, design work left to do

--- AudioInspo/GAIApp.swift
+++ AudioInspo/GAIApp.swift
@@ -3,12 +3,12 @@ import SwiftUI
 @main struct GAIApp: App {
     var body: some Scene {
 
-		// Menu bar item scene for macOS only
+		// TODO: - Menu bar item scene for macOS only
 #if os(macOS)
 
 #endif
         WindowGroup {
-            ContentView()
+			GAIRootView()
         }
     }
 }
---
+++ AudioInspo/Views/GAIRootView.swift
@@ -0,0 +1,18 @@
+//
+//  GAIRootView.swift
+//  AudioInspo
+//
+//  Created by Gale Williams on 9/21/26.
+//
+
+import SwiftUI
+
+struct GAIRootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GAIRootView()
+}
